0c2f2a12f45fb0978753cf9e7f24154e64e3d231
Venkateshwara <venkateshwar.gopisetti@gmail.com>
2021-10-21 01:42:25 +0530

    --no-edit

--- treewalk/test2.py
+++ treewalk/test2.py
@@ -47,43 +47,15 @@ def _walk_directory(root):
 	for root, dirs, files in os.walk(root, topdown=True):
 		walk_list = []
 		for name in dirs:
-			path = os.path.join(root, name)
-			walk_list.append([path.split('\\'), len(path.split('\\'))-1, True])
+			walk_list.append([root, name, len(root.split('\\')), True])
 		for name in files:
-			path = os.path.join(root, name)
-			walk_list.append([path.split('\\'), len(path.split('\\'))-1, False])
-		max_q = len(walk_list)-1
-		walk_list = [x + [max_q] for x in walk_list]
+			walk_list.append([root, name, len(root.split('\\')), False])
 		tree_list += walk_list
 	return tree_list
 
-def _generate_tree(root):
-
-	tree_list = _walk_directory(root)
-	tree_list = _sort_tree(tree_list)
-	counter = {root.strip('\\'):0}
-
-	print(tree_list)
-	for i in tree_list:
-		item, level, is_dir, max_q = i
-		counter_key = ''.join(item[:-1])
-		if counter_key not in counter.keys():
-			counter[counter_key] = 0
-		is_last = counter[counter_key] == max_q
-		# print(CONNECTOR(level))
-
-		bracket = L_BRACKET if is_last else T_BRACKET
-		print(
-			CONNECTOR(level-1)
-			+ bracket
-			+ item[-1]
-			+ (os.sep if is_dir else '')
-		)
-		# print(i)
-		counter[counter_key] += 1
 
 import sys
 
 if __name__=="__main__":
 	dir = sys.argv[1]
-	_generate_tree(dir)
+	print(_walk_directory(dir).sort())
